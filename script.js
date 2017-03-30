5f1af488fa21f2554a48a9a5086377ed1a8abf04
ConnorDoane <2computers73@gmail.com>
2017-03-30 12:57:06 -0600

    assigned

---
+++ script.js
@@ -0,0 +1,36 @@
+var text;
+var listNumber = 0;
+
+$(document).ready(function() {
+	$('#addButton').click(function() {
+		if($('#textbox').val() != '') {
+			text = $('#textbox').val();
+			createListItem();
+			$('#textbox').val('');
+			listNumber++;
+			$('#number').text(listNumber);
+		}
+	})
+	$('#textbox').live('keypress', function(e) {
+		if(e.keyCode == 13) {
+			if($('#textbox').val() != '') {
+				text = $('#textbox').val();
+				createListItem();
+				$('#textbox').val('');
+				listNumber++;
+				$('#number').text(listNumber);
+			}
+		}
+	})
+	$(document).on('click', '.done', function() {
+		$(this).toggleClass('hidden');
+		$(this).parent().addClass('strike');
+		listNumber--;
+		$('#number').text(listNumber);
+	})
+})
+
+
+function createListItem() {
+	$('#list').append('<div class="listItem"><li>' + text + '</li> <button class="done">Done</button></div>');
+}
